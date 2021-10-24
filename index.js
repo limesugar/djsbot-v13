1d47a2e5ae3c775017bf6a9bf8cef30ad9c3cb80
William Hillkirk <Will@Williams-MacBook-Pro.local>
2021-10-24 21:32:37 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,86 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var discord_js_1 = __importStar(require("discord.js"));
+var dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+var client = new discord_js_1.default.Client({
+    intents: [
+        discord_js_1.Intents.FLAGS.GUILDS,
+        discord_js_1.Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+client.on('ready', function () {
+    console.log('the bot is ready!!');
+});
+client.on('messageCreate', function (message) {
+    if (message.content === 'faget') {
+        message.reply({
+            content: 'https://giphy.com/gifs/timanderic-season-3-tim-and-eric-corbs-a5pXfDxlt6VTa',
+        });
+    }
+});
+client.on('messageCreate', function (message) {
+    if (message.content === 'legionfarm players be like') {
+        message.reply({
+            content: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2Flegionfarm&psig=AOvVaw0kCLA3LHlRVlqsdcJz5i2p&ust=1635027585885000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJigu8iG3_MCFQAAAAAdAAAAABAT',
+        });
+    }
+});
+client.on('messageCreate', function (message) {
+    if (message.content === 'potet') {
+        message.reply({
+            content: 'https://tenor.com/view/amberlynn-reid-alr-amberlynn-tongue-out-lick-gif-12445857',
+        });
+    }
+});
+client.on('messageCreate', function (message) {
+    if (message.content === 'but i like u?') {
+        message.reply({
+            content: 'shes mine faget, can u leave',
+        });
+    }
+});
+client.on('messageCreate', function (message) {
+    if (message.content === 'fuck off sloby') {
+        message.reply({
+            content: 'https://tenor.com/view/pretty-raheem-jeep-peace-steal-gif-6040822',
+        });
+    }
+});
+client.on('messageCreate', function (message) {
+    if (message.content === 'ur staying with me woolumy') {
+        message.reply({
+            content: 'https://c.tenor.com/zviA91JGhNIAAAAd/lost-puppy-dog.gif',
+        });
+    }
+});
+client.on('messageCreate', function (message) {
+    if (message.content === 'ur staying with me woolumy') {
+        message.reply({
+            content: 'https://c.tenor.com/zviA91JGhNIAAAAd/lost-puppy-dog.gif',
+        });
+    }
+});
+client.login(process.env.TOKEN);
---
+++ index.ts
@@ -0,0 +1,78 @@
+import discordJS, { Client, Intents } from 'discord.js'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const client = new discordJS.Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+})
+
+client.on('ready', () => {
+    console.log('the bot is ready!!')
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'faget') {
+        message.reply({
+            content: 'https://giphy.com/gifs/timanderic-season-3-tim-and-eric-corbs-a5pXfDxlt6VTa',
+
+        })
+    }
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'legionfarm players be like') {
+        message.reply({
+            content: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Ftwitter.com%2Flegionfarm&psig=AOvVaw0kCLA3LHlRVlqsdcJz5i2p&ust=1635027585885000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCJigu8iG3_MCFQAAAAAdAAAAABAT',
+
+        })
+    }
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'potet') {
+        message.reply({
+            content: 'https://tenor.com/view/amberlynn-reid-alr-amberlynn-tongue-out-lick-gif-12445857',
+
+        })
+    }
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'but i like u?') {
+        message.reply({
+            content: 'shes mine faget, can u leave',
+
+        })
+    }
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'fuck off sloby') {
+        message.reply({
+            content: 'https://tenor.com/view/pretty-raheem-jeep-peace-steal-gif-6040822',
+
+        })
+    }
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ur staying with me woolumy') {
+        message.reply({
+            content: 'https://c.tenor.com/zviA91JGhNIAAAAd/lost-puppy-dog.gif',
+
+        })
+    }
+})
+
+client.on('messageCreate', (message) => {
+    if (message.content === 'ur staying with me woolumy') {
+        message.reply({
+            content: 'https://c.tenor.com/zviA91JGhNIAAAAd/lost-puppy-dog.gif',
+
+        })
+    }
+})
+client.login(process.env.TOKEN)
